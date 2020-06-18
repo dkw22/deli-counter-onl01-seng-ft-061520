073fd761e2f8808cdc6f10798a3f37731fc87b36
Devon Wehrlin <dkwehrlin.99@gmail.com>
2020-06-18 01:28:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,5 +5,6 @@ def line(place)
     puts "The line is currently empty."
   else 
     fill_line = "The line is currently:"
-    place.each.with_index(1)
+    place.each.with_index(1) do |perosn, i|
+      
 end
