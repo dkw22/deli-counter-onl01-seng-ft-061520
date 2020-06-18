70da233fdd65a8b75433f6fa03afc887a20fe99c
Devon Wehrlin <dkwehrlin.99@gmail.com>
2020-06-18 01:21:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 # Write your code here.
 deli = []
 def line(place)
-  if place == 0
+  place == 0
     puts "The line is currently empty."
-  elsif place > 0 
+  place > 0 
     puts "The line is currently #{place}."
   end
 end
